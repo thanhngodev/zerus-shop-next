7d935190414265b2bfb0ad0d0d0ee76107a136d4
thanhngodev <thanhngodev@gmail.com>
2026-02-09 19:25:28 +0700

    Handle apply sanity schema type

--- sanity/schemaTypes/brandTypes.ts
+++ sanity/schemaTypes/brandTypes.ts
@@ -1,43 +1,31 @@
-import { TagIcon } from "lucide-react";
+import { TagIcon } from "@sanity/icons";
 import { defineField, defineType } from "sanity";
 
-export const categoryType = defineType({
-  name: "category",
-  title: "Category",
+export const brandType = defineType({
+  name: "brand",
+  title: "Brand",
   type: "document",
   icon: TagIcon,
   fields: [
     defineField({
       name: "title",
       type: "string",
-      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "slug",
       type: "slug",
       options: {
         source: "title",
-        maxLength: 96,
       },
-      validation: (rule) => rule.required(),
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: "description",
       type: "text",
     }),
     defineField({
-      name: "range",
-      type: "number",
-      description: "Starting from",
-    }),
-    defineField({
-      name: "featured",
-      type: "boolean",
-      initialValue: false,
-    }),
-    defineField({
       name: "image",
-      title: "Category Image",
+      title: "Brand Image",
       type: "image",
       options: {
         hotspot: true,
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,24 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { CategoryType } from './categoryType'
+import { type SchemaTypeDefinition } from "sanity";
+import { addressType } from "./addressType";
+import { authorType } from "./authorType";
+import { blockContentType } from "./blockContentType";
+import { blogCategoryType } from "./blogCategoryType";
+import { blogType } from "./blogType";
+import { brandType } from "./brandTypes";
+import { categoryType } from "./categoryType";
+import { orderType } from "./orderType";
+import { productType } from "./productType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [CategoryType, ],
-}
+  types: [
+    categoryType,
+    blockContentType,
+    productType,
+    orderType,
+    brandType,
+    blogType,
+    blogCategoryType,
+    authorType,
+    addressType,
+  ],
+};
