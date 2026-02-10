afb74017238937722f24e3122613c99354a7bf8c
thanhngodev <thanhngodev@gmail.com>
2026-02-10 23:18:41 +0700

    Handle build ui/ux shop page and filtered

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,5 +1,15 @@
-const ShopPage = () => {
-  return <div>ShopPage</div>;
+import Shop from "@/components/common/Shop";
+import { getAllBrands, getCategories } from "@/sanity/queries";
+
+const ShopPage = async () => {
+  const categories = await getCategories();
+  const brands = await getAllBrands();
+  
+  return (
+    <div className="bg-white">
+      <Shop categories={categories} brands={brands} />
+    </div>
+  );
 };
 
 export default ShopPage;
